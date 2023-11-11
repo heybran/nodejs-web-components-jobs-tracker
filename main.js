1b049cd3d457cdef1b16bb928df05d8ed2b1bd9b
brandon <gwonzhang@gmail.com>
2023-11-11 08:48:48 +0800

    replace breeze-router with cucumber-router and update index.html

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import "./src/index.js";
-import BreezeRouter from "breeze-router";
+import CucumberRouter from "../cucumber-router/src/index.js";
 import "cucumber-components/src/components/side-nav/side-nav.js";
 import "cucumber-components/src/components/side-nav-item/side-nav-item.js";
 import "cucumber-components/src/components/icon/icon.js";
@@ -13,40 +13,38 @@ import "cucumber-components/src/components/textarea/textarea.js";
 import "cucumber-components/src/components/checkbox/checkbox.js";
 import "cucumber-components/src/components/dialog/dialog.js";
 import "cucumber-components/src/components/button/button.js";
+import "cucumber-components/src/components/spinner/spinner.js";
 
-const ROUTER = new BreezeRouter();
+const outlet = document.querySelector('[router-outlet]');
+const ROUTER = new CucumberRouter(outlet);
 window.ROUTER = ROUTER;
 
-const main = document.querySelector('#main');
-
-ROUTER.add('/', async ({ route, params }) => {
-  await import("./src/routes/add-job.js").catch(console.error);
-  main.innerHTML = `<add-job></add-job>`;
-});
-
-ROUTER.add('/jobs', async ({ route, params }) => {
-  await import("./src/routes/jobs-list.js").catch(console.error);
-  main.innerHTML = `<jobs-list></jobs-list>`;
-  // https://developer.mozilla.org/en-US/docs/Web/API/CustomElementRegistry/getName
-  // not supported yet
-  // console.log(customElements.getName(signup));
-});
-
-ROUTER.add('/jobs/edit', async ({ route, params }) => {
-  await import("./src/routes/edit-job.js").catch(console.error);
-  const editJob = document.createElement('edit-job');
-  main.appendChild(editJob);
-});
-
-ROUTER.add('/jobs/delete', async ({ route, params }) => {
-  await import("./src/routes/delete-job.js").catch(console.error);
-  const deleteJob = document.createElement('delete-job');
-  main.appendChild(deleteJob);
-});
-
-ROUTER.add('404', () => {
-  alert('page not found');
-  ROUTER.redirect('/');
-});
 
 ROUTER.start();
+
+// ROUTER.add('/jobs', async ({ route, params }) => {
+//   await import("./src/routes/jobs-list.js").catch(console.error);
+//   main.innerHTML = `<jobs-list></jobs-list>`;
+//   // https://developer.mozilla.org/en-US/docs/Web/API/CustomElementRegistry/getName
+//   // not supported yet
+//   // console.log(customElements.getName(signup));
+// });
+
+// ROUTER.add('/jobs/edit', async ({ route, params }) => {
+//   await import("./src/routes/edit-job.js").catch(console.error);
+//   const editJob = document.createElement('edit-job');
+//   main.appendChild(editJob);
+// });
+
+// ROUTER.add('/jobs/delete', async ({ route, params }) => {
+//   await import("./src/routes/delete-job.js").catch(console.error);
+//   const deleteJob = document.createElement('delete-job');
+//   main.appendChild(deleteJob);
+// });
+
+// ROUTER.add('404', () => {
+//   alert('page not found');
+//   ROUTER.redirect('/');
+// });
+
+// ROUTER.start();
